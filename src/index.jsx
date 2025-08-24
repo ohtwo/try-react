4df4ae3a5706ce001262ad7ab89af687629da2f1
ohtwo <hello@tinygram.dev>
2025-08-24 21:02:08 +0900

    wip

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import Square from "./App.jsx";
+import Board from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Square />
+    <Board />
   </StrictMode>,
 );
