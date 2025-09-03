21ad68919fce0fa3c05353034599ab8dc17f4913
ohtwo <hello@tinygram.dev>
2025-09-03 23:42:31 +0900

    37

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 import CourseGoal from "./components/CourseGoal.tsx";
-import "./App.css";
-import reactImg from "./assets/react.svg";
 import Header from "./components/Header.tsx";
+import reactImg from "./assets/react.svg";
+import "./App.css";
+
+type CourseGoal = {
+  id: number;
+  title: string;
+  description: string;
+};
 
 function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {}
+
   return (
     <main>
       <Header image={{ src: reactImg, alt: "React logo" }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal title="Learn React + TS">
         <p>Learn it from scratch</p>
       </CourseGoal>
