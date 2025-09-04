fedbc8eea0fbcc154789b498e5ebb7bd94990346
ohtwo <hello@tinygram.dev>
2025-09-04 20:42:38 +0900

    43

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,19 +1,25 @@
-import type { FormEvent } from "react";
+import { type FormEvent, useRef } from "react";
 
 export default function NewGoal() {
-  function handleSumit(event: FormEvent) {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+  function handleSumit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const _goal = goal.current!.value;
+    const _summary = summary.current!.value;
   }
 
   return (
     <form onSubmit={handleSumit}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" ref={goal} />
       </p>
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" ref={summary} />
       </p>
       <p>
         <button>Add Goal</button>
