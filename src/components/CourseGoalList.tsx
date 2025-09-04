597d4ec61ccf6695b60b2c36c9c058e71256271d
ohtwo <hello@tinygram.dev>
2025-09-04 17:56:18 +0900

    40

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -4,14 +4,18 @@ import { type CourseGoal as Goal } from "../App.tsx";
 
 type CourseGoalListProps = PropsWithChildren<{
   goals: Goal[];
+  onDeleteGoal: (id: number) => void;
 }>;
 
-export default function CourseGoalList({ goals }: CourseGoalListProps) {
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
   return (
     <ul>
       {goals.map((goal) => (
         <li key={goal.id}>
-          <CourseGoal title={goal.title}>
+          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
             <p>{goal.description}</p>
           </CourseGoal>
         </li>
