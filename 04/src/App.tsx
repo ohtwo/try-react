ec33071e03963d421279ff1cea17b90ee56ca007
ohtwo <hello@tinygram.dev>
2025-09-08 18:51:54 +0900

    59

--- 04/src/App.tsx
+++ 04/src/App.tsx
@@ -1,11 +1,17 @@
-import { useRef } from "react";
 import Input from "./components/Input.tsx";
+import Form from "./components/Form.tsx";
+import Button from "./components/Button.tsx";
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input label={"Test"} id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
