ac48cde2d7586141c55b5f4a72fbc755e4e2a33f
ohtwo <hello@tinygram.dev>
2025-09-08 20:13:16 +0900

    61

--- 04/src/components/Form.tsx
+++ 04/src/components/Form.tsx
@@ -1,20 +1,45 @@
-import type { ComponentPropsWithoutRef, FormEvent } from "react";
+import {
+  type ComponentPropsWithoutRef,
+  type FormEvent,
+  forwardRef,
+  useImperativeHandle,
+  useRef
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...props }: FormProps) {
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+const Form = forwardRef<FormHandle, FormProps>(
+  ({ onSave, children, ...props }, ref) => {
+    const form = useRef<HTMLFormElement>(null);
 
-    const formData = new FormData(event.currentTarget);
-    const data = Object.fromEntries(formData);
-    onSave(data);
-  }
-  return (
-    <form onSubmit={handleSubmit} {...props}>
-      {children}
-    </form>
-  );
-}
+    useImperativeHandle(ref, () => {
+      return {
+        clear: () => {
+          console.log("CLEARING");
+          form.current?.reset();
+        },
+      };
+    });
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+      const data = Object.fromEntries(formData);
+      onSave(data);
+    }
+    return (
+      <form onSubmit={handleSubmit} {...props} ref={form}>
+        {children}
+      </form>
+    );
+  },
+);
+
+export default Form;
