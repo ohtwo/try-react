b4c04d22e81271adb96aec47af65a3ecee357df8
ohtwo <hello@tinygram.dev>
2025-09-10 07:54:14 +0900

    77

--- 06-side-effects/src/components/Timer.tsx
+++ 06-side-effects/src/components/Timer.tsx
@@ -1,15 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1_000);
 
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
   useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 50);
     }, 50);
+    interval.current = timer;
+
+    return () => clearInterval(timer);
   }, []);
 
   const formattedRemainingTime = (remainingTime / 1_000).toFixed(2);
