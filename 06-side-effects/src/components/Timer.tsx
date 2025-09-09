2c52d2a6129ac7ece426feaf0444ad39ef400ce0
ohtwo <hello@tinygram.dev>
2025-09-10 03:53:39 +0900

    75

--- 06-side-effects/src/components/Timer.tsx
+++ 06-side-effects/src/components/Timer.tsx
@@ -1,11 +1,21 @@
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import { useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1_000);
+
+  setInterval(() => {
+    setRemainingTime((prevTime) => prevTime - 50);
+  }, 50);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1_000} value={remainingTime} />
+      </p>
+      <p>{}</p>
     </Container>
   );
 }
