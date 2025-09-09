cbcc539b8375bd30f59b0b1a28b409c62d12f0bc
ohtwo <hello@tinygram.dev>
2025-09-09 13:16:11 +0900

    67

--- 05-context-reducer/src/App.tsx
+++ 05-context-reducer/src/App.tsx
@@ -1,15 +1,18 @@
-import AddTimer from './components/AddTimer.tsx';
-import Header from './components/Header.tsx';
-import Timers from './components/Timers.tsx';
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
+import TimersContextProvider from "./store/timers-context.tsx";
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <AddTimer />
-        <Timers />
-      </main>
+      <TimersContextProvider>
+        <Header />
+        <main>
+          <AddTimer />
+          <Timers />
+        </main>
+      </TimersContextProvider>
     </>
   );
 }
--- 05-context-reducer/src/store/timers-context.tsx
+++ 05-context-reducer/src/store/timers-context.tsx
@@ -1,19 +1,38 @@
-import { createContext } from "react";
+import { createContext, type ReactNode } from "react";
 
 type Timer = {
   name: string;
   duration: number;
-}
+};
 
 type TimersState = {
   isRunning: boolean;
   timers: Timer[];
-}
+};
 
 type TimersContextValue = TimersState & {
-  addTimer: (timer: Timer) => void,
-  startTimers: () => void,
-  stopTimers: () => void,
-}
+  addTimer: (timer: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
 
 const TimersContext = createContext<TimersContextValue | null>(null);
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: false,
+    addTimer: (timer) => {},
+    startTimers: () => {},
+    stopTimers: () => {},
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
