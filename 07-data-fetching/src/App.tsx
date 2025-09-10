10c521cb7cbbcce46e34bf6a8f2d78d1c2bf4dd7
ohtwo <hello@tinygram.dev>
2025-09-10 15:37:47 +0900

    82

--- 07-data-fetching/src/App.tsx
+++ 07-data-fetching/src/App.tsx
@@ -1,5 +1,49 @@
+import { type ReactNode, useEffect, useState } from "react";
+
+import BlogPosts, { type BlogPost } from "./components/BlogPosts.tsx";
+import { get } from "./util/http.ts";
+import fetchingImg from "./assets/data-fetching.png";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts",
+      )) as RawDataBlogPost[];
+
+      const posts = data.map((post) => ({
+        id: post.id,
+        title: post.title,
+        text: post.body,
+      }));
+
+      setFetchedPosts(posts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="Fetching..." />
+      {content}
+    </main>
+  );
 }
 
 export default App;
