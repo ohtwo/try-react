b8b4e3bc94d77e63ae56046138ebabec6921ece6
ohtwo <hello@tinygram.dev>
2025-09-11 14:03:51 +0900

    85

--- 07-data-fetching/src/App.tsx
+++ 07-data-fetching/src/App.tsx
@@ -3,6 +3,7 @@ import { type ReactNode, useEffect, useState } from "react";
 import BlogPosts, { type BlogPost } from "./components/BlogPosts.tsx";
 import { get } from "./util/http.ts";
 import fetchingImg from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage.tsx";
 
 type RawDataBlogPost = {
   id: number;
@@ -13,20 +14,32 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts",
-      )) as RawDataBlogPost[];
+      setIsFetching(true);
 
-      const posts = data.map((post) => ({
-        id: post.id,
-        title: post.title,
-        text: post.body,
-      }));
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts",
+        )) as RawDataBlogPost[];
 
-      setFetchedPosts(posts);
+        const posts = data.map((post) => ({
+          id: post.id,
+          title: post.title,
+          text: post.body,
+        }));
+
+        setFetchedPosts(posts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -34,8 +47,16 @@ function App() {
 
   let content: ReactNode;
 
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts...</p>;
   }
 
   return (
