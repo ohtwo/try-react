5fc0a557d85b7f8d000a5bd7e4cb5c6061b01574
ohtwo <hello@tinygram.dev>
2025-09-11 17:14:39 +0900

    92

--- 08-redux/src/store/cart-slice.ts
+++ 08-redux/src/store/cart-slice.ts
@@ -22,7 +22,25 @@ export const cartSlice = createSlice({
     addToCart(
       state,
       action: PayloadAction<{ id: string; title: string; price: number }>,
-    ) {},
-    removeFromCart() {},
+    ) {
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+
+      if (index >= 0) {
+        state.items[index].quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      const index = state.items.findIndex((item) => item.id === action.payload);
+
+      if (state.items[index].quantity === 1) {
+        state.items.splice(index, 1);
+      } else {
+        state.items[index].quantity--;
+      }
+    },
   },
 });
